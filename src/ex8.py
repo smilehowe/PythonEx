ebb459adff0d1e5cf25a1943a18eab0779e22d11
smilehowe <smilefrank@foxmail.com>
2014-03-01 16:55:18 +0800

    commit ex1-8

---
+++ src/ex8.py
@@ -0,0 +1,13 @@
+#-*-coding:gbk-*-
+
+formatter = "%r %r %s %r"
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "Èý", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+"I had this thing.",
+"That you could type up right.",
+"But it didn't sing.",
+"So I said goodnight."
+)
